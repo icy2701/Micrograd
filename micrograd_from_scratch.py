286b03c351fc70442e088602575429c31115e16b
icy2701 <aisiri2701@gmail.com>
2026-04-01 15:24:30 +0530

    initial commit

---
+++ micrograd_from_scratch.py
@@ -0,0 +1,15 @@
+import math
+import numpy as np
+import matplotlib.pyplot as pyplot
+
+
+def f(x):
+    return 3*x**2 - 4*x +5 
+
+
+print(f(3.0))
+
+xs=np.arange(-5,5,0.25)
+print(xs)
+ys=f(xs)
+print(ys)
